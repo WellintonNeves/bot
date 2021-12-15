ec97ade1a3de46e81c075e648b23355ef7d75210
Wellinton Neves Lucena Junior <60451977+wjr1506@users.noreply.github.com>
2021-12-15 16:19:11 -0300

    att7.4

--- index.js
+++ index.js
@@ -5,49 +5,7 @@ const bodyParser = require('body-parser');
 
 // NodeJS bot
 
-const Insta = require('./insta.js');
-const client = new Insta.Client();
 
-app.use(bodyParser.urlencoded({extended:true}))
-
-
-client.on('connected', () => {
-  console.log(`${client.user.username} Is Ready Now For Chats`);
-});
-
-
-client.on('messageCreate', (message) => {
-
-  //visualizar mensagem
-
-  if (message.author.id === client.user.id) return
-  message.markSeen();
-
-  //capturar mensagem via url
-
-  addUrl(`https://node-red-yrbsr-2021-11-03.mybluemix.net/instagram?mensage=${message}&usuario=${message.author.fullName}&session=${client.user}`)
-  .then(res => res.json())
-    .then(async json => {
-    for (var i = 0; i < json.length; i++) {
-
-    //enviar primeira remessa de mensagens
-
-    await message.chat.sendMessage(json[i].text || json[i].title);
-    console.log(json[i].text || json[i].title);
-
-    for (var key in json[i]) {
-      if (json[i][key].length == 3) {
-        for (var j = 0; j < json[i][key].length; j++) {
-            //enviar opções
-            message.chat.sendMessage(json[i][key][j].label)
-            console.log(json[i][key][j].label);
-          }
-        } 
-      }
-    }   
-  })
-})
-client.login('w_jr01', 'Wjr1506@');
 
 //end code
 
